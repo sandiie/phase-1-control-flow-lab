3be033ebaaba5d41ee1e9fa2ab0ab27e297a6cd3
+ <+>
2024-03-19 17:10:24 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+  return `This one is on me!`;}
+
+  else if(ride > 2500){
+    return `No can do.`;
+  }
+
+  else if(ride > 2000){
+    return `I will gladly take your thirty bucks.`;
+  }
+return `That will be twenty bucks.`;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? `Ok, sounds good.` : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+  
+  case 'not as generous':
+  return 'Thank you.';
+  default: 
+  return 'Bye.';
+  }
 }
